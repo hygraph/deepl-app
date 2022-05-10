a808c3a526111a95a1f02fbc5ec68a871197bfbe
Cadu de Castro Alves <cadudecastroalves@gmail.com>
2022-05-10 15:02:15 +0200

    Fix code

--- pages/form-sidebar.tsx
+++ pages/form-sidebar.tsx
@@ -1,10 +1,26 @@
-import { useApp, Wrapper } from "@graphcms/app-sdk-react";
+import { useApp, useFormSidebarExtension, Wrapper } from "@graphcms/app-sdk-react";
 
 export default function FormSidebar() {
-    console.log('sidebar:', useApp());
     return (
         <Wrapper>
-            <button>Translate</button>
+            <SidebarElement />
         </Wrapper>
+    )
+}
+
+const SidebarElement = () => {
+    const { extension, form } = useFormSidebarExtension();
+    console.log('extension:', extension);
+    console.log('form:', form);
+    console.log('app:', useApp());
+
+    async function handleTranslate() {
+        const titleField = await form.getFieldState('localization_en.title');
+        console.log('Title:', titleField?.value);
+        
+    }
+
+    return (
+        <button onClick={handleTranslate}>Translate</button>
     )
 }
